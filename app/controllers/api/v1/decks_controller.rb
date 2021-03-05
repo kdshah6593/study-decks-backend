cf8c96c2a28bd407980dca8c63e6c70894563fda
Kunal <kdshah6593@gmail.com>
2021-03-05 13:03:42 -0500

    added other deck routes and controller action skeletons

--- app/controllers/api/v1/decks_controller.rb
+++ app/controllers/api/v1/decks_controller.rb
@@ -5,4 +5,32 @@ class Api::V1::DecksController < ApplicationController
         render json: decks
     end
 
+    def show
+        deck = Deck.find_by(id: params[:id])
+        render json: deck
+    end
+
+    def create
+        deck = Deck.new(deck_params)
+        if deck.save
+            render json: deck, status: :accepted
+        else
+            render json: {errors: deck.errors.full_messages}, status: :unprocessable_entity
+            # add validation in Deck Model for title presence
+        end
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+    def deck_params
+        params.require(:deck).permit(:title, :user_id)
+    end
+
 end
