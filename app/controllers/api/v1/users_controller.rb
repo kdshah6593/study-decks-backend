ee1eba271c6866fd6c32162dd71cbba9a55df6a2
Kunal <kdshah6593@gmail.com>
2021-03-05 15:53:38 -0500

    user show/create route and controller action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,4 +4,19 @@ class Api::V1::UsersController < ApplicationController
         users = User.all
         render json: UserSerializer.new(users)
     end
+
+    def show
+        user = User.find_by(id: params[:id])
+        render json: UserSerializer.new(user)
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: UserSerializer.new(user), status: :accepted
+        else
+            render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+            # add validation in user Model
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :decks
       resources :flashcards
-      resources :users, only: [:index]
+      resources :users, only: [:index, :show, :create]
     end
   end
 
