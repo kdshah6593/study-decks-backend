7e92defbde49f133ee5acdcc352e42611c5bcdab
Kunal <kdshah6593@gmail.com>
2021-03-09 17:56:09 -0500

    implement create user functionality

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,10 +13,14 @@ class Api::V1::UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
-            render json: UserSerializer.new(user), status: :accepted
+            render json: UserSerializer.new(user), status: :created
         else
-            render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
-            # add validation in user Model
+            render json: {errors: user.errors.full_messages}, status: :not_acceptable
         end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :password)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
+    
     has_many :decks, dependent: :destroy
     has_many :flashcards, through: :decks
 end
