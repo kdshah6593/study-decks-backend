c249d4ff0cbb5271e17a32d91e2ffdecf48ad304
Kunal <kdshah6593@gmail.com>
2021-03-10 10:49:29 -0500

    implement jwt in create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
 
     def index
         users = User.all
@@ -11,9 +12,10 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def create
-        user = User.new(user_params)
-        if user.save
-            render json: UserSerializer.new(user), status: :created
+        @user = User.create(user_params)
+        if @user.valid?
+            @token = encode_token(user_id: @user.id)
+            render json: UserSerializer.new(@user), status: :created
         else
             render json: {errors: user.errors.full_messages}, status: :not_acceptable
         end
