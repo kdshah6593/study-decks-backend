ec7edbdb14a8379ca524b46da429fb5845169792
Kunal <kdshah6593@gmail.com>
2021-03-10 16:16:06 -0500

    error handling login signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
     validates :username, presence: true
     validates :username, uniqueness: true
     validates :password, presence: true
+    validates :password, length: { in: 6..20 }, on: :create
 end
